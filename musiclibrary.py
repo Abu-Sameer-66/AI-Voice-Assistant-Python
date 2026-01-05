574ae6ca0c9d3376d59ee7c257013277ce8a4731
Abu-Sameer <sameerdataanalyst66@gmail.com>
2026-01-05 22:49:21 +0500

    Create musiclibrary.py

---
+++ musiclibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "heer": "https://www.youtube.com/watch?v=5R4UChPzRBc&list=RD5R4UChPzRBc&start_radio=1",
+    "sang e mah": "https://www.youtube.com/watch?v=BbdYw5XLe-c&list=RDBbdYw5XLe-c&start_radio=1",
+    "favourite": "https://www.youtube.com/watch?v=RDWbgH_feR0&list=RDRDWbgH_feR0&start_radio=1&rv=BbdYw5XLe-c",
+    "jhol": "https://www.youtube.com/watch?v=-2RAq5o5pwc&list=RD5R4UChPzRBc&index=3"
+}
